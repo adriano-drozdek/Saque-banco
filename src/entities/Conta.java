1ae05c216e5eb7b462a0dfbc0aef533736863347
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2021-11-27 16:13:52 -0300

    Adicionando construtores e Getters e Setters

---
+++ src/entities/Conta.java
@@ -0,0 +1,46 @@
+package entities;
+
+public class Conta {
+
+	private String nome;
+	private int conta;
+	private int valor;
+	
+	public Conta(String nome, int conta, int valor) {
+		super();
+		this.nome = nome;
+		this.conta = conta;
+		this.valor = valor;
+	}
+
+	public Conta(String nome, int conta) {
+		super();
+		this.nome = nome;
+		this.conta = conta;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getConta() {
+		return conta;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+	
+	public void deposito(double quantidade) {
+		valor += quantidade;
+	}
+	
+	public void saque(double quantidade) {
+		valor -= quantidade + 5.00;
+	}
+	
+}
